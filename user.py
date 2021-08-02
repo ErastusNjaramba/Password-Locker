e0015fd8a2757660bd225078a5a0d57700128495
ErastusNjaramba <erastus.kimani@student.moringaschool.com>
2021-08-02 09:33:36 +0300

    add user details and init function

---
+++ user.py
@@ -0,0 +1,13 @@
+class user:
+  """
+  class that generates new instance of user
+  """
+
+  user_user =[] #Empty user list
+
+  def __init__(self,user_name,password):
+
+    # docstring removed for simplicity
+
+      self.user_name = user_name
+      self.user_name = password
